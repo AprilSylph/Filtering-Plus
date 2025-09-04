d9aa79546fdbccb378f2f8c72ae71f263670fc52
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2025-09-04 10:39:50 +0100

    chore(deps): upgrade to eslint v9; migrate to neostandard (#22)

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import neostandard from 'neostandard';
+
+/**
+ * Semistandard style guide, via neostandard package.
+ * @see https://github.com/neostandard/neostandard?tab=readme-ov-file#readme
+ */
+export default neostandard({ env: ['browser', 'webextensions'], semi: true });
